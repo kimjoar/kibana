a997de5b00ff22e6644dd6cdea83d26283200ed7
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2018-01-30 13:35:41 +0100

    eslint overrides for src/cli tests

---
+++ src/cli/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: '../../.eslintrc',
+  overrides: [
+    {
+      files: ['**/__tests__/**/*.js'],
+      plugins: [
+        'jest',
+      ],
+
+      env: {
+        'jest/globals': true,
+      },
+
+      rules: {
+        'jest/no-focused-tests': 'error',
+        'jest/no-identical-title': 'error',
+      },
+    }
+  ]
+};
